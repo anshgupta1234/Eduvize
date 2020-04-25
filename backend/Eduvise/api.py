21a741728905cbca756b99970c8b8feeba913d30
anshgupta1234 <ansh.guptaca.2004@gmail.com>
2020-04-25 12:33:33 -0700

    .khan_api

--- backend/Eduvise/api.py
+++ backend/Eduvise/api.py
@@ -1,5 +1,5 @@
 import json
-from khan_api import KhanAcademySignIn, KhanAPI
+from .khan_api import KhanAcademySignIn, KhanAPI
 from flask import (
     Blueprint, render_template, request, url_for, redirect
 )
