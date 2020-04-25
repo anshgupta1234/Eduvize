4263b6fee57426ece60fce74e1d2d44ff7deb589
Jainil Shah <jainilushah@gmail.com>
2020-04-24 22:24:52 -0400

    Database & ORM

--- backend/Eduvise/__init__.py
+++ backend/Eduvise/__init__.py
@@ -1,14 +1,26 @@
 import os
 
 from flask import Flask, render_template, send_from_directory
-from . import api
+from flask_mongoengine import MongoEngine
+db = MongoEngine()
+
+from . import api, auth, models, webviews
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    db.init_app(app)
+    with open("Eduvise/secrets.txt") as f:
+        lines = f.readlines()
+        secret_key = lines[0].strip()
+        mongo_pw = lines[1].strip()
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        static_url_path='static/'
+        SECRET_KEY='secret_key',
+        static_url_path='static/',
+        MONGODB_SETTINGS={
+            'db': 'Eduvise',
+            'host': 'mongodb+srv://admin:'+mongo_pw+'@eduvise-f0zco.gcp.mongodb.net/test?retryWrites=true&w=majority'
+        }
     )
 
     if test_config is None:
@@ -24,9 +36,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
-    def index():
-        return render_template("index.html")
-        
     app.register_blueprint(api.bp)
+    app.register_blueprint(webviews.bp)
     return app
