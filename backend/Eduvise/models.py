36e5361ee63b837c9583ba4da117092d92564306
Jainil Shah <jainilushah@gmail.com>
2020-04-25 19:10:13 -0400

    nonstrict db

--- backend/Eduvise/models.py
+++ backend/Eduvise/models.py
@@ -3,6 +3,7 @@ from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Document):
+    meta={"strict": False}
     email = db.StringField(max_length=50, unique=True)
     password = db.StringField()
     display_name = db.StringField()
@@ -11,7 +12,7 @@ class User(UserMixin, db.Document):
     khan_secret = db.StringField()
     points = db.IntField(default=0)
     total_points = db.IntField(default=0)
-
+    
     def set_password(self, password):
         """Create hashed password."""
         self.password = generate_password_hash(password, method='sha256')
