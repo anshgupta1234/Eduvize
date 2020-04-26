c7d8c06fe9eaac80b8b00853b3b67913204dbc54
Eric Andrechek <ericandrechek@gmail.com>
2020-04-25 21:58:55 -0400

    i hope work

--- backend/Eduvise/khan_api.py
+++ backend/Eduvise/khan_api.py
@@ -32,13 +32,13 @@ class KhanAcademySignIn:
         request_token, request_token_secret = self.service.get_request_token(
             params={"oauth_callback": self.get_callback_url()}, method="POST"
         )
-        session["request_token"] = request_token
-        session["request_token_secret"] = request_token_secret
+        self.session["request_token"] = request_token
+        self.session["request_token_secret"] = request_token_secret
         return request_token, request_token_secret, self.service.get_authorize_url(request_token)
 
     def callback(self):
-        request_token = session.pop("request_token")
-        request_token_secret = session.pop("request_token_secret")
+        request_token = self.session.pop("request_token")
+        request_token_secret = self.session.pop("request_token_secret")
 
         if "oauth_verifier" not in request.args:
             return None, None, None
